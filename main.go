24af914c113e38cc137e9e95ab50ab6145172062
Yuta-Haruna <50592688+Yuta-Haruna@users.noreply.github.com>
2023-07-23 17:18:07 +0900

    🚧 電卓画面の構築中

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("GoodNight World")
+	myApp := app.New()
+	myWindow := myApp.NewWindow("GoodNight")
+
+	// ボタンを作成
+	button := widget.NewButton("クリックしてください", func() {
+		// ボタンがクリックされたときに実行する処理をここに書きます
+		// 例：ウィンドウにメッセージを表示する
+		myWindow.SetContent(widget.NewLabel("ボタンがクリックされました！"))
+	})
+
+	// ボタンをウィンドウに配置
+	myWindow.SetContent(container.NewVBox(button))
+
+	// ウィンドウを表示
+	myWindow.ShowAndRun()
 }
