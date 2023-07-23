5c5a68e59849e815477637781bdf623255610575
Yuta-Haruna <50592688+Yuta-Haruna@users.noreply.github.com>
2023-07-23 17:32:27 +0900

    🚧 Fyne機能検証_画面テスト途中

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ func main() {
 	myWindow := myApp.NewWindow("GoodNight")
 
 	// ボタンを作成
-	button := widget.NewButton("クリックしてください", func() {
+	button := widget.NewButton("Click Here", func() {
 		// ボタンがクリックされたときに実行する処理をここに書きます
 		// 例：ウィンドウにメッセージを表示する
-		myWindow.SetContent(widget.NewLabel("ボタンがクリックされました！"))
+		myWindow.SetContent(widget.NewLabel("Button Click!"))
 	})
 
 	// ボタンをウィンドウに配置
